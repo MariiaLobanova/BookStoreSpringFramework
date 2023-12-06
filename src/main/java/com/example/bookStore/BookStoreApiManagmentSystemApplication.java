fd6c64eab6c5d7e513ad82c7f6992d41f81df059
Lobanova_Mariia <lobanovmariia@gmail.com>
2023-12-06 11:48:44 +0100

    resolve the problem after testing in main.class

--- src/main/java/com/example/bookStore/BookStoreApiManagmentSystemApplication.java
+++ src/main/java/com/example/bookStore/BookStoreApiManagmentSystemApplication.java
@@ -1,5 +1,7 @@
 package com.example.bookStore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -7,16 +9,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class BookStoreApiManagmentSystemApplication {
-
-	public static void main(String[] args) {
-
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-		BookDao bookDao =context.getBean(BookDao.class);
-
-		BookController bookController = context.getBean(BookController.class);
-
+	@SpringBootApplication(scanBasePackages = "com.example.bookStore")
+	public class BookStoreApiManagmentSystemApplication {
+		public static void main(String[] args) {
+			SpringApplication.run(BookStoreApiManagmentSystemApplication.class, args);
+		}
 	}
 
-}
